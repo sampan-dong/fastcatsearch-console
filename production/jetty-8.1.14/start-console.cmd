77a73b5255e0ef1664ad101d4e6742220b47ccff
freshbread <whitelen24@gmail.com>
2015-07-30 19:08:21 +0900

    윈도우에서 사전 키워드 파일로 업로드 시 UTF-8 인코딩으로 된 텍스트 파일이 올라가지 않고 EUC-KR 인코딩이 된 파일이 업로드되는 문제 수정

--- production/jetty-8.1.14/start-console.cmd
+++ production/jetty-8.1.14/start-console.cmd
@@ -5,4 +5,4 @@ echo fastcatsearch-console start. see logs/server.log file.
 
 REM SET JAVA_PATH=C:\Program Files\Java\jdk1.6.0_29\bin\
 
-"%JAVA_PATH%java.exe" -jar start.jar>>logs/server.log 2>&1
+"%JAVA_PATH%java.exe" -Dfile.encoding=UTF-8 -jar start.jar>>logs/server.log 2>&1
