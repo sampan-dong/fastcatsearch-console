1f9eafb3dcc5c7f14b2fb680a8af29ed7d7dac01
FreshBread <jhjeon@fastcat.co>
2016-03-10 15:15:28 +0900

    XSSFilter 필터 적용이 login.html, doLogin.html에서만 적용되도록 수정

--- src/main/java/org/fastcatsearch/console/web/xss/XSSFilter.java
+++ src/main/java/org/fastcatsearch/console/web/xss/XSSFilter.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 /**
  * Created by 전제현 on 2016-03-08.
@@ -19,9 +20,25 @@ public class XSSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        filterChain.doFilter(new XSSRequestWrapper((HttpServletRequest) servletRequest), servletResponse);
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        if (includeUrl(request)) {
+            filterChain.doFilter(new XSSRequestWrapper((HttpServletRequest) servletRequest), servletResponse);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
     }
 
     @Override
     public void destroy() {}
+
+    private boolean includeUrl(HttpServletRequest request) {
+        String uri = request.getRequestURI().toString().trim();
+        if(uri.endsWith("ogin.html")){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
