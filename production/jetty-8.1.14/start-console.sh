ee51d44a817940747757cb4c602aa2f2c6ae89bb
Sang Song <swsong@fastcat.co>
2016-04-01 14:50:58 +0900

    too many form key 에러 대처방안.

--- production/jetty-8.1.14/start-console.sh
+++ production/jetty-8.1.14/start-console.sh
@@ -6,6 +6,7 @@ if [ ! -d "temp" ]; then
 	mkdir temp
 fi
 
-java -Dfile.encoding=UTF-8 -jar $SERVER_HOME/start.jar > $SERVER_HOME/logs/server.log 2>&1 &
+# 스키마에 필드가 많아서 저장시 too many keys 에러가 발생한다면 org.eclipse.jetty.server.Request.maxFormKeys 수치를 올려준다.
+java -Dorg.eclipse.jetty.server.Request.maxFormKeys=2000 -Dfile.encoding=UTF-8 -jar $SERVER_HOME/start.jar > $SERVER_HOME/logs/server.log 2>&1 &
 
 echo fastcatsearch-console start. see logs/server.log file.
